806788e1361695ac11685574895ba0da7beb4fe1
Sathish Kumar <msathish.tsm@gmail.com>
2017-12-28 21:01:38 +0530

    Tar issue fixed

--- File_collect.py
+++ File_collect.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python
 import os
 import commands
-import glob
-import tarfile
-import re
-
-print("This script used to collect required files for PR")
-
-print("""Script options:
-        1. Config session issue
-        2. Speific process logs
-        3. PM/CM data collectioni
-        4. Exit""")
-option=int(input("Please Enter your Option : "))
-
-log_path="/opt/mycom/logs/"
-temp_path="/opt/nfs/sathish/scripts/tmp_s/"
-#process=['Transform','Load','Write','DIMgr']
-process=['Transform']
-def config_log():
+import glob                                                                                                                                                                                                                                  
+import tarfile                                                                                                                                                                                                                               
+import re                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                             
+print("This script used to collect required files for PR")                                                                                                                                                                                   
+                                                                                                                                                                                                                                             
+print("""Script options:                                                                                                                                                                                                                     
+        1. Config session issue                                                                                                                                                                                                              
+        2. Speific process logs                                                                                                                                                                                                              
+        3. PM/CM data collectioni                                                                                                                                                                                                            
+        4. Exit""")                                                                                                                                                                                                                          
+option=int(input("Please Enter your Option : "))                                                                                                                                                                                             
+                                                                                                                                                                                                                                             
+log_path="/opt/mycom/logs/"                                                                                                                                                                                                                  
+temp_path="/opt/nfs/sathish/scripts/tmp_s/"                                                                                                                                                                                                  
+#process=['Transform','Load','Write','DIMgr']                                                                                                                                                                                                
+process=['Transform','Load']                                                                                                                                                                                                                 
+file_t=[]                                                                                                                                                                                                                                    
+def config_log():                                                                                                                                                                                                                            
         print("config related files will be collected")
         date_c=int(input("Enter date (Ex:YYYYMMDD) :"))
         time_s=input("Enter start time of config session (Ex:HH):")
@@ -29,15 +30,22 @@ def config_log():
 def pm_cm():
         print("PM/CM files will be collected")
 
-file_t=[]
 def append1(file1):
         for i in file1:
                 file_t.append(i)
-def tar_file(tar):
-        tar1 = tarfile.open("Transform_log.tar", "w")
+def tar_file(tar,date,proc):
+        tar1 = tarfile.open("{0}_log_{1}.tar".format(proc,date), "w")
         for name in (tar):
                 tar1.add(name)
-                tar1.close()
+        tar1.close()
+        tar=[]
+
+def print_t(list1):
+        count=0
+        for i in sorted(list1):
+                print(i)
+                count +=1
+        print("Total files : ",count)
 
 def process_log(process_c,date,start_time,end_time):
     print(process_c,date,start_time,end_time)
@@ -47,13 +55,14 @@ def process_log(process_c,date,start_time,end_time):
         print("If statement")
         file1=glob.glob("{0}{1}*{2}0[{3}-{4}]*".format(log_path,process_c,date,start_time,end_time))
         append1(file1)
+        print(file1)
         for i in range(len(file1)):
                 data=re.findall(r"[\w']+", file1[i])
                 value1=str("/{0}/{1}/{2}/{3}.{4}.{5}".format(data[0],data[1],data[2],data[3],data[4],data[5]))
                 val_l.append(value1)
         append1(list(set(val_l)))
+        tar_file(file_t,date,process_c)
         print_t(file_t)
-        tar_file(file_t)
     elif (len(str(start_time)) == 1) & (len(str(end_time)) == 2):
         print("1st elIf statement")
         c=str(end_time)
@@ -74,6 +83,7 @@ def process_log(process_c,date,start_time,end_time):
                 val_l.append(value1)
         append1(list(set(val_l)))
         print_t(file_t)
+        tar_file(file_t,date)
     elif (len(str(start_time)) == 2) & (len(str(end_time)) == 2):
         print("2nd elIf statement")
         c=str(end_time)
@@ -96,13 +106,7 @@ def process_log(process_c,date,start_time,end_time):
                 val_l.append(value1)
         append1(list(set(val_l)))
         print_t(file_t)
-
-def print_t(list1):
-        count=0
-        for i in sorted(list1):
-                print(i)
-                count +=1
-        print("Total files : ",count)
+        tar_file(file_t,date)
 
 if option == 1:
         config_log()
