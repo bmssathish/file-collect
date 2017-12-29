141420c85a288139a728f5ff59d83e7b044c5e70
Sathish Kumar <msathish.tsm@gmail.com>
2017-12-29 17:53:39 +0530

    file collection fun almost completed with .bz2

--- File_collect.py
+++ File_collect.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python
 import os
 import commands
-import glob                                                                                                                                                                                                                                  
-import tarfile                                                                                                                                                                                                                               
-import re                                                                                                                                                                                                                                    
-                                                                                                                                                                                                                                             
-print("This script used to collect required files for PR")                                                                                                                                                                                   
-                                                                                                                                                                                                                                             
-print("""Script options:                                                                                                                                                                                                                     
-        1. Config session issue                                                                                                                                                                                                              
-        2. Speific process logs                                                                                                                                                                                                              
-        3. PM/CM data collectioni                                                                                                                                                                                                            
-        4. Exit""")                                                                                                                                                                                                                          
-option=int(input("Please Enter your Option : "))                                                                                                                                                                                             
-                                                                                                                                                                                                                                             
-log_path="/opt/mycom/logs/"                                                                                                                                                                                                                  
-temp_path="/opt/nfs/sathish/scripts/tmp_s/"                                                                                                                                                                                                  
-#process=['Transform','Load','Write','DIMgr']                                                                                                                                                                                                
-process=['Transform','Load']                                                                                                                                                                                                                 
-file_t=[]                                                                                                                                                                                                                                    
-def config_log():                                                                                                                                                                                                                            
+import glob
+import tarfile
+import re
+
+print("This script used to collect required files for PR")
+
+print("""Script options:
+        1. Config session issue
+        2. Speific process logs
+        3. PM/CM data collectioni
+        4. Exit""")
+option=int(input("Please Enter your Option : "))
+
+log_path="/opt/mycom/logs/"
+temp_path="/opt/nfs/sathish/scripts/tmp_s/"
+process=['Transform','Load','Write','DIMgr']
+#process=['Transform','Load']
+file_t=[]
+empt_list=[]
+def config_log():
         print("config related files will be collected")
         date_c=int(input("Enter date (Ex:YYYYMMDD) :"))
         time_s=input("Enter start time of config session (Ex:HH):")
@@ -34,11 +35,10 @@ def append1(file1):
         for i in file1:
                 file_t.append(i)
 def tar_file(tar,date,proc):
-        tar1 = tarfile.open("{0}_log_{1}.tar".format(proc,date), "w")
+        tar1 = tarfile.open("{0}_log_{1}.tar.bz2".format(proc,date), "w:bz2")
         for name in (tar):
                 tar1.add(name)
         tar1.close()
-        tar=[]
 
 def print_t(list1):
         count=0
@@ -51,6 +51,7 @@ def process_log(process_c,date,start_time,end_time):
     print(process_c,date,start_time,end_time)
     print(len(str(start_time)),len(str(end_time)))
     val_l=[]
+    global file_t
     if len(str(start_time)) == 1 & len(str(end_time)) ==1:
         print("If statement")
         file1=glob.glob("{0}{1}*{2}0[{3}-{4}]*".format(log_path,process_c,date,start_time,end_time))
@@ -63,6 +64,7 @@ def process_log(process_c,date,start_time,end_time):
         append1(list(set(val_l)))
         tar_file(file_t,date,process_c)
         print_t(file_t)
+        file_t=list(empt_list)
     elif (len(str(start_time)) == 1) & (len(str(end_time)) == 2):
         print("1st elIf statement")
         c=str(end_time)
@@ -82,8 +84,9 @@ def process_log(process_c,date,start_time,end_time):
                 value1=str("/{0}/{1}/{2}/{3}.{4}.{5}".format(data[0],data[1],data[2],data[3],data[4],data[5]))
                 val_l.append(value1)
         append1(list(set(val_l)))
+        tar_file(file_t,date,process_c)
         print_t(file_t)
-        tar_file(file_t,date)
+        file_t=list(empt_list)
     elif (len(str(start_time)) == 2) & (len(str(end_time)) == 2):
         print("2nd elIf statement")
         c=str(end_time)
@@ -98,15 +101,16 @@ def process_log(process_c,date,start_time,end_time):
         elif (int(s[0]) == 1) & (int(c[0]) == 2):
             file1=glob.glob("{0}{1}*{2}1[{3}-9]*".format(log_path,process_c,date,int(s[1]),int(c[1])))
             append1(file1)
-            file1=glob.glob("{0}{1}*{2}2[{3}-{4}]*".format(log_path,process_c,date,int(s[1]),int(c[1])))
+            file1=glob.glob("{0}{1}*{2}2[0-{4}]*".format(log_path,process_c,date,int(s[1]),int(c[1])))
             append1(file1)
         for i in range(len(file1)):
                 data=re.findall(r"[\w']+", file1[i])
                 value1=str("/{0}/{1}/{2}/{3}.{4}.{5}".format(data[0],data[1],data[2],data[3],data[4],data[5]))
                 val_l.append(value1)
         append1(list(set(val_l)))
+        tar_file(file_t,date,process_c)
         print_t(file_t)
-        tar_file(file_t,date)
+        file_t=list(empt_list)
 
 if option == 1:
         config_log()
