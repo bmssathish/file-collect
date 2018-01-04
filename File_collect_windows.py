9e3be3951c219f61508fafb6b646e73317a263c7
Sathish Kumar <34186270+bmssathish@users.noreply.github.com>
2018-01-04 23:42:54 +0530

    Add files via upload

--- File_collect_windows.py
+++ File_collect_windows.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 import glob
 import tarfile
 import re
@@ -7,15 +6,15 @@ print("This script used to collect required files for PR")
 
 print("""Script options:
         1. Config session issue
-        2. Speific process logs
-        3. PM/CM data collectioni
+        2. Specific process logs
+        3. PM/CM data collection
         4. Exit""")
 option = int(input("Please Enter your Option : "))
 
-log_path = "/opt/mycom/logs/"
+log_path = "F:/Sat/github/file-collect-master/"
 temp_path = "/opt/nfs/sathish/scripts/tmp_s/"
-process = ['Transform', 'Load', 'Write', 'DIMgr']
-# process=['Transform','Load']
+# process = ['Transform', 'Load', 'Write', 'DIMgr']
+process = ['Transform']
 file_t = []
 empt_list = []
 
@@ -57,20 +56,30 @@ def process_log(process_c, date, start_time, end_time):
     print(process_c, date, start_time, end_time)
     print(len(str(start_time)), len(str(end_time)))
     val_l = []
+    file1 = []
+    file_list_stack = []
+    file_list_gc = []
     global file_t
     if len(str(start_time)) == 1 & len(str(end_time)) == 1:
-        print("If statement")
+        print("Collecting ", process_c, "logs... ")
         file1 = glob.glob("{0}{1}*{2}0[{3}-{4}]*".format(log_path, process_c, date, start_time, end_time))
         append1(file1)
         print(file1)
         for i in range(len(file1)):
+                # This will remove all special char and split into words
                 data = re.findall(r"[\w']+", file1[i])
                 value1 = str("/{0}/{1}/{2}/{3}.{4}.{5}".format(data[0], data[1], data[2], data[3], data[4], data[5]))
                 val_l.append(value1)
         append1(list(set(val_l)))
-        tar_file(file_t, date, process_c)
+#        tar_file(file_t, date, process_c)
         print_t(file_t)
         file_t = list(empt_list)
+        print("Collecting ", process_c, " Stack traces logs... ")
+        file_list_stack = glob.glob("{0}mycom{1}.sh.*{2}0[{3}-{4}]*".format(log_path, process_c.lower(), date, start_time, end_time))
+        print(file_list_stack)
+        print("Collecting ", process_c, "GC logs... ")
+        file_list_gc = glob.glob("{0}mycom{1}_gc.*{2}0[{3}-{4}]*".format(log_path, process_c.lower(), date, start_time, end_time))
+        print(file_list_gc)
     elif (len(str(start_time)) == 1) & (len(str(end_time)) == 2):
         print("1st elIf statement")
         c = str(end_time)
